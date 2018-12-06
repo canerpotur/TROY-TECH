e7232e954faf12c9d8802433334d038ebeb01fbc
Kağan Özaslan <knozaslan@gmail.com>
2018-12-06 20:44:45 +0300

    Update sketch_dec06a.ino

--- Searches/Wireless Transmission/APC220/6.12_demo_codes/transmitter_code/sketch_dec06a/sketch_dec06a.ino
+++ Searches/Wireless Transmission/APC220/6.12_demo_codes/transmitter_code/sketch_dec06a/sketch_dec06a.ino
@@ -4,23 +4,25 @@
 
 SoftwareSerial mySerial(4, 5); // RX, TX
 
-uint8_t value;
+int value=123;
+int value1=0;
+int value2=44;
+
 void setup()
 {
-  Serial.begin(9600); // make sure serial monitor is set to this rate
-  mySerial.begin(9600); // change this to the rate set in rf magic.
+  mySerial.begin(1200); // change this to the rate set in rf magic.
 }
 
 void loop()
 {
-    value=0xA3;
-    delay(300);
+    
     mySerial.println(value);
+    delay(250);
 
+    mySerial.println(value2);
+    delay(250);
 
-    value=0xB7;
-    mySerial.println(value);
-    delay(300);
-  
+    mySerial.println(value1);
+    delay(250);
 
 }
