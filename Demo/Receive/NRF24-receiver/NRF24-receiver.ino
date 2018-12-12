ea7f5d6411d1ea2ceeb93bae5392617c93053be6
Caner Potur <canerpotur@gmail.com>
2018-12-12 15:14:39 +0300

    Update NRF24-receiver.ino

--- Demo/Receive/NRF24-receiver/NRF24-receiver.ino
+++ Demo/Receive/NRF24-receiver/NRF24-receiver.ino
@@ -3,7 +3,7 @@
 #include <RF24.h>
 
 RF24 radio(7,8);
-
+ uint8_t text;
 const byte address[6] = "00001";
 
 void setup() {
@@ -11,18 +11,64 @@ void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0,address);
+  
   radio.setPALevel(RF24_PA_MAX);
   radio.setDataRate(RF24_250KBPS);
   radio.startListening();
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
 }
 
 void loop() {
-  uint8_t text;
-  
   if (radio.available()) {
-    radio.read(&text, sizeof(text));
-       Serial.print(text, HEX);
+radio.read(&text, sizeof(text));
+ if(text!=0b10000)   
+ {
+    
+       Serial.print(text, BIN);
        Serial.println("....."); 
-       
+ }
+else
+Serial.println("Wait for command..");
+
+
+if(bitRead(text,0)) 
+digitalWrite(2,HIGH);
+else 
+digitalWrite(2,LOW);
+
+if(bitRead(text,1)) 
+digitalWrite(3,HIGH);
+else 
+digitalWrite(3,LOW);
+
+if(bitRead(text,2)) 
+digitalWrite(4,HIGH);
+else 
+digitalWrite(4,LOW);
+
+if(bitRead(text,3)) 
+digitalWrite(5,HIGH);
+else 
+digitalWrite(5,LOW);
+
+
   }
+
+/*else
+{
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  
+  
+  
+  
+  }
+
+*/
+  
 }
