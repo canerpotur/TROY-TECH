e6a5f331e13d080a272cdc48c46a4115c85ef033
Caner Potur <canerpotur@gmail.com>
2018-12-15 19:19:27 +0300

    Demo-transmitter

--- Demo/Transmit/NRF24-transmitter_with_analogs/NRF24-transmitter_with_analogs.ino
+++ Demo/Transmit/NRF24-transmitter_with_analogs/NRF24-transmitter_with_analogs.ino
@@ -6,12 +6,13 @@
 
 RF24 radio(9,8);
 
-const byte address[6] = "00001";
-uint8_t but_mem = 0b10000;
+const byte address[6] = "7";
+uint8_t but_mem = 50;
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
+  radio.setPayloadSize(1);
   radio.openWritingPipe(address);
   radio.setAutoAck(false);
   //radio.setRetries(15, 15); 
@@ -20,38 +21,69 @@ void setup() {
   radio.stopListening();
   
 
-  PSX.mode(PSX_PAD1, MODE_DIGITAL, MODE_LOCK);
+  PSX.mode(PSX_PAD1, MODE_ANALOG, MODE_LOCK);
   PSX.updateState(PSX_PAD1);
 }
 
 void joystick_read()
 {
   PSX.updateState(PSX_PAD1);  
-  if (PRESSED_TRIANGLE(PSX_PAD1)) {
-    but_mem |= 0b10001;
+  
+  if (ANALOG_LEFT_Y(PSX_PAD1)<10) {
+     but_mem = 23;
    }
-  if (RELEASED_TRIANGLE(PSX_PAD1)) {
-    but_mem &= 0b11110;
-  }    
-  if (PRESSED_SQUARE(PSX_PAD1)) {
-    but_mem |= 0b11000; 
-  }
-  if (RELEASED_SQUARE(PSX_PAD1)) {
-    but_mem &= 0b10111;
-  }
-  if (PRESSED_CROSS(PSX_PAD1)) {
-    but_mem |= 0b10100;
-  }
-  if (RELEASED_CROSS(PSX_PAD1)) {
-    but_mem &= 0b11011;
-  }
-  if (PRESSED_CIRCLE(PSX_PAD1)) {
-    but_mem |= 0b10010;
-  }
-  if (RELEASED_CIRCLE(PSX_PAD1)) {
-    but_mem &= 0b11101;
-  }
-  Serial.println(but_mem, BIN);
+
+  if (ANALOG_LEFT_Y(PSX_PAD1)>9 && ANALOG_LEFT_Y(PSX_PAD1)<50) {
+     but_mem = 22;
+   }
+
+  if (ANALOG_LEFT_Y(PSX_PAD1)>49 && ANALOG_LEFT_Y(PSX_PAD1)<95) {
+     but_mem = 21;
+   }
+
+  if (ANALOG_LEFT_Y(PSX_PAD1)>94 && ANALOG_LEFT_Y(PSX_PAD1)<141) {
+     but_mem = 50;
+   }
+
+  if (ANALOG_LEFT_Y(PSX_PAD1)>140 && ANALOG_LEFT_Y(PSX_PAD1)<190) {
+     but_mem = 11;
+   }
+
+  if (ANALOG_LEFT_Y(PSX_PAD1)>189 && ANALOG_LEFT_Y(PSX_PAD1)<246) {
+     but_mem = 12;
+   }
+
+  if (ANALOG_LEFT_Y(PSX_PAD1)>245 && ANALOG_LEFT_Y(PSX_PAD1)<256) {
+     but_mem = 13;
+   }
+/******* right analog ******/
+//if( but_mem != 50)
+//{
+if (ANALOG_RIGHT_X(PSX_PAD1)<10) {
+     but_mem = 43;
+   }
+
+  if (ANALOG_RIGHT_X(PSX_PAD1)>9 && ANALOG_RIGHT_X(PSX_PAD1)<50) {
+     but_mem = 42;
+   }
+
+  if (ANALOG_RIGHT_X(PSX_PAD1)>49 && ANALOG_RIGHT_X(PSX_PAD1)<100) {
+     but_mem = 41;
+   }
+
+  if (ANALOG_RIGHT_X(PSX_PAD1)>150 && ANALOG_RIGHT_X(PSX_PAD1)<200) {
+     but_mem = 31;
+   }
+
+  if (ANALOG_RIGHT_X(PSX_PAD1)>199 && ANALOG_RIGHT_X(PSX_PAD1)<246) {
+     but_mem = 32;
+   }
+
+  if (ANALOG_RIGHT_X(PSX_PAD1)>245 && ANALOG_RIGHT_X(PSX_PAD1)<256) {
+     but_mem = 33;
+   }
+ 
+  Serial.println(but_mem);
 
 }
 
