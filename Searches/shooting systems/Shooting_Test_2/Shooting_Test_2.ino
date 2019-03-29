caa25809786b4e896ab19b6337bcda26642c5108
Kağan Özaslan <knozaslan@gmail.com>
2019-03-29 19:39:21 +0300

    Update Shooting_Test_2.ino

--- Searches/shooting systems/Shooting_Test_2/Shooting_Test_2.ino
+++ Searches/shooting systems/Shooting_Test_2/Shooting_Test_2.ino
@@ -9,21 +9,36 @@
 */
 
 // digital pin 2 has a pushbutton attached to it. Give it a name:
-int pushButton = 2;
+  int kaan;
+  const int in1 = 7;     // Motor sürücümüze bağladığımız pinleri tanımlıyoruz
+  const int in2 = 6;  
+  const int in3 = 5;  
 
 // the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // make the pushbutton's pin an input:
-  pinMode(pushButton, INPUT);
+  pinMode(in1, OUTPUT);  //Tüm pinlerden güç çıkışı olacağı için OUTPUT olarak ayarladık.
+  pinMode(in2, OUTPUT); 
+  pinMode(in3, OUTPUT);  
+  
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input pin:
-  int buttonState = digitalRead(pushButton);
-  // print out the state of the button:
-  Serial.println(buttonState);
-  delay(1);        // delay in between reads for stability
+    digitalWrite(in1,  LOW);  
+    digitalWrite(in3, HIGH);
+  kaan = Serial.read();
+  if(kaan != -1)
+  {
+  
+  digitalWrite(in2, HIGH);
+  Serial.println(kaan);
+  delay(120); 
+  digitalWrite(in2, LOW);
+  }
+  delay(10);        
+ /* 
+  */
 }
